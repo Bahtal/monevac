cd970f2861ee17c9334980c7d0404a163072b2a2
Bahtal <bahtal777artem@gmail.com>
2024-11-10 23:59:41 +0300

    footer and form

---
+++ js/main.js
@@ -0,0 +1,56 @@
+document.addEventListener("DOMContentLoaded", function() {
+	const form = document.getElementById('section-form');
+	const username = document.getElementById('section-username');
+	const phone = document.getElementById('section-phone');
+	const message = document.getElementById('section-message');
+	const label = document.querySelectorAll('.section-form__label')
+
+	form.addEventListener('submit', function(e) {
+		e.preventDefault();
+		let isValid = true;
+
+		if (username.value.trim() === '') {
+			username.classList.add('error');
+			label[0].style.opacity = "1";
+			isValid = false;
+		} else {
+			username.classList.remove('error');
+			label[0].style.opacity = "0";
+		}
+
+		if (phone.value.trim() === '') {
+			phone.classList.add('error');
+			label[1].style.opacity = "1";
+			isValid = false;
+		} else {
+			phone.classList.remove('error');
+			label[1].style.opacity = "0";
+		}
+
+		if (message.value.trim() === '') {
+			message.classList.add('error');
+			label[2].style.opacity = "1";
+			isValid = false;
+		} else {
+			message.classList.remove('error');
+			label[2].style.opacity = "0";
+		}
+
+		if (isValid) {
+			alert("Форма успешно отправлена");
+		}
+	});
+
+	IMask(phone, {
+		mask: '+{7}(000)000-00-00',
+		lazy: false,
+		placeholderChar: '_' 
+	});
+	if (phoneInput.value.trim() !== "") {
+    phoneInput.style.color = "#262633"
+} else {
+    phoneInput.style.color = "#0202c7"
+}
+});
+
+
